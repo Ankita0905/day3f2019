e1b2a4d294d1992d0f72a359e5f0a2aaafca8e87
macstudent <ankita.jain12342@gmail.com>
2019-09-20 18:43:19 -0400

    string manupulation 2

--- src/main/java/com/lambton/StringHandling.java
+++ src/main/java/com/lambton/StringHandling.java
@@ -45,17 +45,18 @@ public class StringHandling
         System.out.println(s1);
         s1.toUpperCase();
         System.out.println(s1);
+        System.out.println(s1.concat(s5));
         System.out.println(s5.toLowerCase());
         System.out.println(s5.toUpperCase());
-        System.out.println(s5.charAt(4));
-        System.out.println(s1.compareTo(s4));
-        System.out.println(s1.concat(s5));
         System.out.println(s5.length());
+        System.out.println(s5.contains("College"));
         System.out.println(s5.lastIndexOf("Toronto"));
         System.out.println(s5.replace("Toronto","New York"));
         System.out.println(s5.substring(11,20));
         System.out.println(s5.toCharArray());
         System.out.println(s5.concat("265 Yorkland Blvd., North York"));
+
+
 
     }
     }
